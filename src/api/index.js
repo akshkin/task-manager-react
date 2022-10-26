d17c128857137c3f796abb676fd9878bad2cc576
akshkin <107682289+akshkin@users.noreply.github.com>
2022-10-26 21:10:08 +0200

    changes in api file

--- src/api/index.js
+++ src/api/index.js
@@ -9,22 +9,22 @@ API.interceptors.request.use(req => {
   return req
 })
 
-export const fetchTasks = () => API.get(`/api/tasks`)
+export const fetchTasks = () => API.get(`/tasks`)
 
-export const createTask = newTask => API.post(`/api/tasks`, newTask)
+export const createTask = newTask => API.post(`/tasks`, newTask)
 
-export const updateTask = (id, updatedPost) => API.patch(`/api/tasks/${id}`, updatedPost)
+export const updateTask = (id, updatedPost) => API.patch(`/tasks/${id}`, updatedPost)
 
-export const deleteTask = id => API.delete(`/api/tasks/${id}`)
+export const deleteTask = id => API.delete(`/tasks/${id}`)
 
-export const signIn = formFields => API.post(`/api/users/signin`, formFields)
+export const signIn = formFields => API.post(`/users/signin`, formFields)
 
-export const signUp = formFields => API.post(`/api/users/signup`, formFields)
+export const signUp = formFields => API.post(`/users/signup`, formFields)
 
-export const signOut = () => API.post("/api/users/signout")
+export const signOut = () => API.post("/users/signout")
 
-export const getProfile = () => API.get("/api/users/profile")
+export const getProfile = () => API.get("/users/profile")
 
-export const updateProfile = (updatedProfile) => API.patch("/api/users/profile", updatedProfile)
+export const updateProfile = (updatedProfile) => API.patch("/users/profile", updatedProfile)
 
-export const deleteProfile = () => API.delete("/api/users/profile")
+export const deleteProfile = () => API.delete("/users/profile")
